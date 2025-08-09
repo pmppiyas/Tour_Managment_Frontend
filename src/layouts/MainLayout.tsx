ba69679e5f5e91012045fe384be0510d3c0f7df1
PmpPiyas <princemahmudpiyas@gmail.com>
2025-08-09 19:38:07 +0600

    Add Navbar & Footer

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,15 @@
+import Footer from "@/layouts/Footer";
+import Navbar from "@/layouts/Navbar";
+import { Outlet } from "react-router";
+
 function MainLayout() {
   return (
-    <div className="container mx-auto min-h-screen flex items-center justify-center text-4xl font-semibold animate-pulse">
-      Main Layout
+    <div className="container mx-auto min-h-screen flex flex-col items-center justify-center text-4xl font-semibold ">
+      <Navbar></Navbar>
+      <div className="grow-1">
+        <Outlet />
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
